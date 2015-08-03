5ac71b0872dfd3d51d4ff3c3dae42699271e233f
Roman7791 <romeo7113@gmail.com>
2015-08-03 14:11:25 +0300

    collectionViewCell

---
+++ UniversalSharing/UniversalSharing/UniversalSharingApp/CustomViews/MUSCollectionViewCell.h
@@ -0,0 +1,14 @@
+//
+//  MUSCollectionViewCell.h
+//  UniversalSharing
+//
+//  Created by Roman on 8/3/15.
+//  Copyright (c) 2015 Mobindustry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MUSCollectionViewCell : UICollectionViewCell
+@property (weak, nonatomic) IBOutlet UIImageView *photoImageView;// perhaps to do private
+
+@end
