4b79a9e66ceb9bd46940f43bb0a19a4716dcf284
Roman7791 <romeo7113@gmail.com>
2015-09-07 13:59:02 +0300

    .

---
+++ UniversalSharing/UniversalSharing/UniversalSharingApp/Controllers/MUSDitailPostCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  MUSDitailPostCollectionViewController.h
+//  UniversalSharing
+//
+//  Created by Roman on 9/4/15.
+//  Copyright (c) 2015 Mobindustry. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MUSDitailPostCollectionViewController : UICollectionViewController
+
+@end
