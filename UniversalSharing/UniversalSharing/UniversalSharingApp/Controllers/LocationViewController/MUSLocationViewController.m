e82193ce0e8dfba4d74a282eb97190ad745fbe5d
Liptuga Michael <liptugamichael@gmail.com>
2015-09-09 09:27:51 +0300

    Changed CustomMapView

--- UniversalSharing/UniversalSharing/UniversalSharingApp/Controllers/LocationViewController/MUSLocationViewController.m
+++ UniversalSharing/UniversalSharing/UniversalSharingApp/Controllers/LocationViewController/MUSLocationViewController.m
@@ -12,7 +12,7 @@
 #import "MUSLocationManager.h"
 #import "MUSCustomMapView.h"
 
-@interface MUSLocationViewController () <UITableViewDelegate, UITableViewDataSource, UIActionSheetDelegate>
+@interface MUSLocationViewController () <UITableViewDelegate, UITableViewDataSource, UIActionSheetDelegate, MUSCustomMapViewDelegate>
 /*!
  @property
  @abstract initialization by Place objects via block from socialnetwork(facebook or twitter or VK)
@@ -48,6 +48,10 @@
 
 @property (assign, nonatomic) BOOL isOpenTableView;
 
+@property (strong, nonatomic) UITapGestureRecognizer *tapGestureRecognizer;
+
+@property (nonatomic, strong) UIPanGestureRecognizer *panGesture;
+
 @end
 
 @implementation MUSLocationViewController
@@ -55,13 +59,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     [self initiationNavigationBar];
+    [self initiationPanGestureRecognizer];
     self.leftConstraintTableView.constant =  self.view.frame.size.width;
     self.rightConstraintTavbleView.constant = - self.view.frame.size.width;
-    
-    
-    //self.customMapView.hidden = YES;
-    
-    
+    self.tapGestureRecognizer = [[UITapGestureRecognizer alloc] initWithTarget : self action : @selector(tapOnMapView:)];
     // Do any additional setup after loading the view.
 }
 
@@ -89,47 +90,84 @@
     self.navigationItem.rightBarButtonItem = choosePlaceButton;
 }
 
+- (void) initiationPanGestureRecognizer {
+    self.panGesture = [[UIPanGestureRecognizer alloc] initWithTarget:self action:@selector(moveTableView:)];
+    self.panGesture.maximumNumberOfTouches = 1;
+    self.panGesture.minimumNumberOfTouches = 1;
+    [self.view addGestureRecognizer:self.panGesture];
+    [self.view bringSubviewToFront: self.navigationController.navigationBar];
+}
+
 - (void) showCloseTable {
     if (!self.isOpenTableView) {
         [self showTableViewWithPlaces];
-        self.isOpenTableView = YES;
     } else {
         [self closeTableViewWithPlaces];
-        self.isOpenTableView = NO;
     }
 }
 
 - (void) showTableViewWithPlaces {
     self.leftConstraintTableView.constant =  self.view.frame.size.width / 3;
-    self.rightConstraintTavbleView.constant = - (self.view.frame.size.width / 3) * 2;
-    NSLog(@"Table left = %f, right = %f", self.leftConstraintTableView.constant, self.rightConstraintTavbleView.constant);
-    self.leftConstraintCustomMapView.constant = - (self.view.frame.size.width / 3);
+    self.rightConstraintTavbleView.constant = 0;
+    self.leftConstraintCustomMapView.constant = - (self.view.frame.size.width / 3) * 2;
     self.rightConstraintCustomMapView.constant = (self.view.frame.size.width / 3) * 2;
-    NSLog(@"MAP left = %f, right = %f", self.leftConstraintTableView.constant, self.rightConstraintTavbleView.constant);
-
+    
+    [self.customMapView addGestureRecognizer : self.tapGestureRecognizer];
+    
     [UIView animateWithDuration: 0.4  animations:^{
         [self.view layoutIfNeeded];
         [self.view setNeedsLayout];
     }];
     [UIView commitAnimations];
+    self.isOpenTableView = YES;
 }
 
 - (void) closeTableViewWithPlaces {
     self.leftConstraintTableView.constant =  self.view.frame.size.width;
-    self.rightConstraintTavbleView.constant = - self.view.frame.size.width;
+    self.rightConstraintTavbleView.constant = - (self.view.frame.size.width / 3) * 2;
     
     self.leftConstraintCustomMapView.constant = 0;
     self.rightConstraintCustomMapView.constant = 0;
+    [self.customMapView removeGestureRecognizer: self.tapGestureRecognizer];
     
     [UIView animateWithDuration: 0.4  animations:^{
         [self.view layoutIfNeeded];
         [self.view setNeedsLayout];
     }];
     [UIView commitAnimations];
+    self.isOpenTableView = NO;
 }
 
+- (void) tapOnMapView: (UITapGestureRecognizer *)recognizer {
+    [self closeTableViewWithPlaces];
+}
 
+-(void) moveTableView:(UIPanGestureRecognizer *)recognizer
+{
+    CGPoint translation = [recognizer translationInView:self.view];
+    
+    if([(UIPanGestureRecognizer*)recognizer state] == UIGestureRecognizerStateChanged) {
 
+        float movingByX = self.leftConstraintTableView.constant + translation.x;
+        
+        //NSLog(@"MOVING  = %f", movingByX);
+        if ( movingByX > [UIScreen mainScreen].bounds.size.width / 3){
+            self.leftConstraintTableView.constant = movingByX;
+            self.rightConstraintTavbleView.constant = [UIScreen mainScreen].bounds.size.width / 3 - movingByX;
+            self.rightConstraintCustomMapView.constant = [UIScreen mainScreen].bounds.size.width  - movingByX;
+            self.leftConstraintCustomMapView.constant = - [UIScreen mainScreen].bounds.size.width  + movingByX;
+            [recognizer setTranslation:CGPointMake(0,0) inView:self.view];
+        }
+    }
+    
+    if([(UIPanGestureRecognizer*)recognizer state] == UIGestureRecognizerStateEnded) {
+        if (self.rightConstraintCustomMapView.constant > [UIScreen mainScreen].bounds.size.width / 3){
+            [self showTableViewWithPlaces];
+        }else if ( self.rightConstraintCustomMapView.constant <= [UIScreen mainScreen].bounds.size.width / 3){
+            [self closeTableViewWithPlaces];
+        }
+    }
+}
 
 #pragma mark - userCurrentLocation
 
@@ -152,16 +190,20 @@
             __weak MUSLocationViewController *weakSelf = self;
             
             [_currentSocialNetwork obtainArrayOfPlaces:self.currentLocation withComplition:^(NSMutableArray *places, NSError *error) {
-                
-                weakSelf.arrayLocations = places;
-                [weakSelf.tableView reloadData];
-                [weakSelf.customMapView initiationMapView: places withDistance: [weakSelf.stringDistance integerValue]];
+                if (!error) {
+                    weakSelf.arrayLocations = places;
+                    [weakSelf.customMapView initiationMapView: places withDistance: [weakSelf.stringDistance integerValue] andNetworkType: weakSelf.currentSocialNetwork.networkType];
+                    [weakSelf.tableView reloadData];
+                    weakSelf.customMapView.delegate = weakSelf.self;
+                } else {
+                    [self showErrorAlertWithError: error];
+                }
             }];
         }
     }];
 }
 
-#pragma mark - Table view data source
+#pragma mark - UITableViewDataSource
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
     return [self.arrayLocations count];
@@ -173,81 +215,84 @@
     if (!cell) {
         cell = [[UITableViewCell alloc] initWithStyle: UITableViewCellStyleDefault reuseIdentifier: @"LocationCell"];
     }
-    
     /*!
      get object Place and show name of that on tableviewcell
      */
-    
     Place *place = self.arrayLocations[indexPath.row];
     cell.textLabel.text = place.fullName;
-    
-    /*
-     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"LocationCell" forIndexPath:indexPath];
-     if (cell) {
-     Place *place = self.arrayLocations[indexPath.row];
-     cell.textLabel.text = place.fullName;
-     }
-     */
+    //cell.textLabel.text = [NSString stringWithFormat: @"%d) %@", indexPath.row, place.fullName];
+    cell.textLabel.numberOfLines = 0;
+    cell.textLabel.font = [UIFont systemFontOfSize: 16.0];
+
     return cell;
 }
+
+#pragma mark - UITableViewDelegate
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
     /*!
      get chosen object Place and send to shareViewController via block, leave this controller
      */
-    Place *place = self.arrayLocations[indexPath.row];
+    [self obtainPlaceForPost: indexPath.row];
+}
+
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath {
+    if ([self.arrayLocations firstObject]) {
+        Place *currentPlace = [self.arrayLocations objectAtIndex: indexPath.row];
+        
+        CGFloat height = [self findHeightForText: currentPlace.fullName havingWidth: (self.view.frame.size.width / 3) * 2 - 24 andFont: [UIFont systemFontOfSize: 16.0]];
+        return height;
+    }
+    return 38;
+}
+
+- (CGFloat)findHeightForText:(NSString *)text havingWidth:(CGFloat)widthValue andFont:(UIFont *)font
+{
+    CGFloat result = font.pointSize;
+    if (text)
+    {
+        CGSize size;
+        CGRect frame = [text boundingRectWithSize:CGSizeMake(widthValue, 999)
+                                          options:NSStringDrawingUsesLineFragmentOrigin
+                                       attributes:@{NSFontAttributeName:font}
+                                          context:nil];
+        size = CGSizeMake(frame.size.width, frame.size.height);
+        result = MAX(size.height, result); //At least one row
+    }
+    return result + 20;
+}
+
+
+- (void) obtainPlaceForPost : (NSInteger) currentIndex {
+    Place *place = [self.arrayLocations objectAtIndex: currentIndex];
     self.placeComplition(place, nil);
     [self.navigationController popViewControllerAnimated:YES];
     self.navigationController.navigationBar.translucent = YES;
 }
 
-#pragma mark - actionSheet
+#pragma mark - MUSCustomMapViewDelegate
 
-/*!
- @method
- @abstract when button distance is tapped calling actionSheet with offering distance for choice(100,1000,25000)
- @param sender
- */
-- (IBAction)chooseDistance:(id)sender {
-    //[self alertChooseDistanceShow];
-    
-}
-
-- (void) alertChooseDistanceShow {
-    UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle:@"Choose distance location :" delegate:self cancelButtonTitle:@"Cancel" destructiveButtonTitle:nil otherButtonTitles:
-                                  distanceEqual100,
-                                  distanceEqual1000,
-                                  distanceEqual25000,
-                                  nil];
-    [actionSheet showInView:[UIApplication sharedApplication].keyWindow];
-}
-
-- (void)actionSheet:(UIActionSheet *)popup clickedButtonAtIndex:(NSInteger)buttonIndex {
-    switch (buttonIndex) {
-        case 0:
-            self.stringDistance = distanceEqual100;
-            break;
-        case 1:
-            self.stringDistance = distanceEqual1000;
-            break;
-        case 2:
-            self.stringDistance = distanceEqual25000;
-            break;
-        default:
-            return;
+- (void) reloadCustomMapView:(NSInteger) newDistance {
+    if (newDistance > [self.stringDistance integerValue] * 3 && newDistance < [distanceEqual25000 integerValue]) {
+        self.stringDistance = [NSString stringWithFormat: @"%d", newDistance];
+        [self userCurrentLocation];
     }
-    /*!
-     call this method if we have changed the distance
-     */
-    [self userCurrentLocation];
 }
 
+- (void) selectedPlaceForPostByIndex:(NSInteger)index {
+    [self obtainPlaceForPost: index];
+}
 
+#pragma mark - UIAlertView
 
-
-
-
-
+- (void) showErrorAlertWithError : (NSError*) error {
+    UIAlertView *errorAlert = [[UIAlertView alloc] initWithTitle : Error
+                                                         message : [error localizedFailureReason]
+                                                        delegate : nil
+                                               cancelButtonTitle : musAppButtonTitle_OK
+                                               otherButtonTitles : nil];
+    [errorAlert show];
+}
 
 
 
