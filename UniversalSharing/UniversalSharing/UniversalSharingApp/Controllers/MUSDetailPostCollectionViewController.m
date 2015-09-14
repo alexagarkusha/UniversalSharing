425df8435bf780495a7292103d7c6819ac620eff
Roman7791 <romeo7113@gmail.com>
2015-09-14 15:40:21 +0300

    .

--- UniversalSharing/UniversalSharing/UniversalSharingApp/Controllers/MUSDetailPostCollectionViewController.m
+++ UniversalSharing/UniversalSharing/UniversalSharingApp/Controllers/MUSDetailPostCollectionViewController.m
@@ -9,11 +9,13 @@
 #import "MUSDetailPostCollectionViewController.h"
 #import "UIImageView+RoundImage.h"
 #import "UIImageView+MUSLoadImageFromDataBase.h"
+#import "MUSCollectionViewCellForDetailView.h"
 
-
-@interface MUSDetailPostCollectionViewController ()<UICollectionViewDataSource, UICollectionViewDelegate, UIScrollViewDelegate>
+@interface MUSDetailPostCollectionViewController ()<UICollectionViewDataSource, UICollectionViewDelegate, UIScrollViewDelegate,UICollectionViewDelegateFlowLayout>
 @property (strong, nonatomic) NSArray *arrayOfPics;
 @property (strong, nonatomic) SocialNetwork *currentSocialNetwork;
+@property (nonatomic, assign) CGFloat scale;
+
 
 @end
 
@@ -31,19 +33,45 @@ static NSString * const reuseIdentifier = @"Cell";
     [imageView loadImageFromDataBase: _currentSocialNetwork.icon];
     [imageView roundImageView];
     [view addSubview:imageView];
-   self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:view];
+    self.navigationItem.rightBarButtonItem = [[UIBarButtonItem alloc] initWithCustomView:view];
     ///////////////////////////////////////////////////////////////////////////
     self.navigationItem.title = [NSString stringWithFormat:@"%ld from %lu",(long) 1, (unsigned long)[self.arrayOfPics count]];
-
-    [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:reuseIdentifier];
     
-    // Do any additional setup after loading the view.
+    [self.collectionView registerClass:[UICollectionViewCell class] forCellWithReuseIdentifier:reuseIdentifier];
+    ////////////////////////////////////////////////////////////////////////////////////////////////////
+    self.scale = 10.0;
+     [self.collectionView setPagingEnabled:YES];
+    [self.view layoutIfNeeded];
+    
+    
+//    UIPinchGestureRecognizer *gesture = [[UIPinchGestureRecognizer alloc] initWithTarget:self action:@selector(didReceivePinchGesture:)];
+//    [self.collectionView addGestureRecognizer:gesture];
+    
 }
 
+- (void)didReceivePinchGesture:(UIPinchGestureRecognizer*)gesture
+{
+    static CGFloat scaleStart;
+    
+    if (gesture.state == UIGestureRecognizerStateBegan)
+    {
+        scaleStart = self.scale;
+    }
+    else if (gesture.state == UIGestureRecognizerStateChanged)
+    {
+        self.scale = scaleStart * gesture.scale;
+        //[self.collectionView.collectionViewLayout invalidateLayout];
+//        CGRect visibleRect = (CGRect){.origin = self.collectionView.contentOffset, .size = self.collectionView.bounds.size};
+//        CGPoint visiblePoint = CGPointMake(CGRectGetMidX(visibleRect), CGRectGetMidY(visibleRect));
+//        NSIndexPath *visibleIndexPath = [self.collectionView indexPathForItemAtPoint:visiblePoint];
+//        UICollectionViewCell *currentCell =[self.collectionView cellForItemAtIndexPath:visibleIndexPath];
+//        currentCell.backgroundView.frame = CGRectMake(currentCell.backgroundView.frame.origin.x-self.scale,currentCell.backgroundView.frame.origin.y-self.scale, currentCell.backgroundView.frame.size.width+self.scale, currentCell.backgroundView.frame.size.height+self.scale);
+    }
+}
 
 - (void) setObjectsWithArray :(NSArray*) arrayOfPics andCurrentSocialNetwork :(id)currentSocialNetwork {
     self.arrayOfPics = arrayOfPics;
-    self.currentSocialNetwork = currentSocialNetwork;    
+    self.currentSocialNetwork = currentSocialNetwork;
 }
 
 #pragma mark <UICollectionViewDataSource>
@@ -57,10 +85,21 @@ static NSString * const reuseIdentifier = @"Cell";
 }
 
 - (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
-    UICollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:reuseIdentifier forIndexPath:indexPath];
-    cell.backgroundView = [[UIImageView alloc] initWithImage:self.arrayOfPics[indexPath.row]];
-        //self.collectionView nu
+    MUSCollectionViewCellForDetailView *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"detailCell" forIndexPath:indexPath];
+    
+    [cell setImageContext:self.arrayOfPics[indexPath.row]];
+    [self.collectionView addGestureRecognizer:cell.scrollView.pinchGestureRecognizer];
+    [self.collectionView addGestureRecognizer:cell.scrollView.panGestureRecognizer];
+
     return cell;
+}
+
+- (void)collectionView:(UICollectionView *)collectionView
+  didEndDisplayingCell:(MUSCollectionViewCellForDetailView *)cell
+    forItemAtIndexPath:(NSIndexPath *)indexPath {
+    
+    [self.collectionView removeGestureRecognizer:cell.scrollView.pinchGestureRecognizer];
+    [self.collectionView removeGestureRecognizer:cell.scrollView.panGestureRecognizer];
 }
 
 - (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
@@ -68,11 +107,11 @@ static NSString * const reuseIdentifier = @"Cell";
     CGPoint visiblePoint = CGPointMake(CGRectGetMidX(visibleRect), CGRectGetMidY(visibleRect));
     NSIndexPath *visibleIndexPath = [self.collectionView indexPathForItemAtPoint:visiblePoint];
     self.navigationItem.title = [NSString stringWithFormat:@"%ld from %lu",(long)visibleIndexPath.row + 1, (unsigned long)[self.arrayOfPics count]];
-
+    
 }
 
 - (CGSize)collectionView:(UICollectionView *)collectionView layout:(UICollectionViewLayout*)collectionViewLayout sizeForItemAtIndexPath:(NSIndexPath *)indexPath {
-    return CGSizeMake(self.view.frame.size.width, self.view.frame.size.height -100);
+    return CGSizeMake(self.view.frame.size.width , self.view.frame.size.height - 100);
 }
 
 @end
