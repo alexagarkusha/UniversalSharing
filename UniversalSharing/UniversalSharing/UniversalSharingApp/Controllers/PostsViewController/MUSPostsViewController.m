7ea380257a5fecb423a008c149b9c78e84c0944b
Liptuga Michael <liptugamichael@gmail.com>
2015-09-24 14:01:36 +0300

    Fixed bug @"Select all posts" and @"Deselect posts"

--- UniversalSharing/UniversalSharing/UniversalSharingApp/Controllers/PostsViewController/MUSPostsViewController.m
+++ UniversalSharing/UniversalSharing/UniversalSharingApp/Controllers/PostsViewController/MUSPostsViewController.m
@@ -340,6 +340,7 @@
 
 - (void) editingTableView {
     [super setEditing:YES animated:YES];
+    [self.selectAllButton setTitle: @"Select All"];
     [self.editButton setTitle:doneButtonTitle];
     [self.navigationItem.leftBarButtonItem setEnabled:YES];
     [self.navigationItem.leftBarButtonItem setTintColor: nil];
@@ -352,11 +353,13 @@
     [_toolBar setHidden:NO];
 }
 
+#pragma mark - MUSPostCellDelegate
 
 - (void) addIndexToIndexSetWithCell:(MUSPostCell*)cell {
     NSIndexPath *indexPath = [self.tableView indexPathForCell:cell];
     [self addIndexToIndexSet: indexPath];
 }
+
 
 - (void) addIndexToIndexSet :(NSIndexPath*) indexPath {
     if ([self.mutableIndexSet containsIndex:indexPath.row]) {
@@ -369,6 +372,9 @@
     }
     [self.mutableIndexSet addIndex:indexPath.row];
     self.barButtonDeletePost.enabled = YES;
+    if ([self.mutableIndexSet count] == [self.arrayPosts count]) {
+        [self.selectAllButton setTitle: @"Deselect All"];
+    }
 }
 
 - (IBAction) buttonSelectAllTapped:(id)sender {
