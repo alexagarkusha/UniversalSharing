cc272d7ec9ace462a7ad87cf1439737f85f985cd
Roman7791 <romeo7113@gmail.com>
2015-10-14 17:37:13 +0300

    MUSPopUpTableViewCell is okay

--- UniversalSharing/UniversalSharing/UniversalSharingApp/CustomViews/MUSPopUpTableViewCell.m
+++ UniversalSharing/UniversalSharing/UniversalSharingApp/CustomViews/MUSPopUpTableViewCell.m
@@ -9,6 +9,7 @@
 #import "MUSPopUpTableViewCell.h"
 
 @interface MUSPopUpTableViewCell ()
+
 @property (weak, nonatomic) IBOutlet UIImageView *imageViewNetwork;
 @property (weak, nonatomic) IBOutlet UISwitch *switchButton;
 
@@ -40,7 +41,6 @@
 - (void) configurationPopUpTableViewCellWith: (SocialNetwork*) socialNetwork andReason:(ReasonType) currentReason {
     self.imageViewNetwork.image = [UIImage imageNamed:socialNetwork.icon];
     self.switchButton.tag = socialNetwork.networkType;
-    
     if (!socialNetwork.isLogin || currentReason == MUSConnect) {
         [self.switchButton setOn:NO animated:YES];
         self.switchButton.enabled = NO;
@@ -51,6 +51,7 @@
         self.switchButton.enabled = YES;
     }
 }
+
 - (IBAction)switchEvent:(UISwitch*)sender {
     if (!sender.isOn) {
         sender.backgroundColor = [UIColor whiteColor];
